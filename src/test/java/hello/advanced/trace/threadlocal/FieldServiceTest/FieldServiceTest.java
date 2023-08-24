e0c67058b3199d18b8dbebce9ea5962e1c38f74e
jeongho <qkrjh0904@gmail.com>
2023-08-25 00:17:31 +0900

    feat: 동시성 문제가 발생하는 테스트 작성

---
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest/FieldServiceTest.java
@@ -0,0 +1,69 @@
+package hello.advanced.trace.threadlocal.FieldServiceTest;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+
+    @Test
+    @DisplayName("동시성 문제가 없는 이름 저장 테스트")
+    public void noConcurrencySaveNameTest() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(2000);    // 동시성 문제 발생 X
+        threadB.start();
+        sleep(3000);    // 메인 쓰레드 종료 대기
+
+        log.info("main exit");
+    }
+
+    @Test
+    @DisplayName("동시성 문제가 발생하는 이름 저장 테스트")
+    public void concurrencySaveNameTest() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(100);    // 동시성 문제 발생
+        threadB.start();
+        sleep(3000);    // 메인 쓰레드 종료 대기
+
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
