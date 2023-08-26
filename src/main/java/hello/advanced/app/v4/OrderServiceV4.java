b4624acba375fb34f9de3a61c5f7015fda8e64ad
jeongho <qkrjh0904@gmail.com>
2023-08-26 23:33:57 +0900

    feat: ThreadLocal 사용해서 학습 테스트 작성

---
+++ src/main/java/hello/advanced/app/v4/OrderServiceV4.java
@@ -0,0 +1,27 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV4 {
+
+    private final OrderRepositoryV4 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderServiceV4.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져줘야한다.
+        }
+    }
+
+}
